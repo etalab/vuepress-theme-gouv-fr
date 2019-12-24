c0324ed2ec6941ebfa3f34f940d878f21d489cd3
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-12-24 16:11:09 +0100

    Support sidebar titles

---
+++ layouts/Layout.vue
@@ -0,0 +1,34 @@
+<template>
+  <ParentLayout>
+    <template #sidebar-top v-if="sidebarTitle">
+      <h2 class="sidebar-title" v-text="sidebarTitle" />
+    </template>
+  </ParentLayout>
+</template>
+
+<script>
+import ParentLayout from '@parent-theme/layouts/Layout.vue'
+
+export default {
+  name: 'Layout',
+  components: {
+    ParentLayout,
+  },
+  computed: {
+    sidebarTitle: function () {
+      for (let [group, title] of Object.entries(this.$site.themeConfig.sidebarTitles || {})) {
+        if (this.$page.path.startsWith(group)) {
+          return title
+        }
+      }
+      return null
+    }
+  }
+}
+</script>
+<style lang="stylus">
+.sidebar-title {
+  padding-left 1.25rem
+  padding-bottom 0
+}
+</style>
