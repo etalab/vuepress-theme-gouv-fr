6e3a53b943672f562bf4885428cb0e7e84553d04
Antoine Augusti <antoine.augusti@data.gouv.fr>
2020-01-31 10:44:53 +0100

    Add sidebar titles for sections #10
    * Support sidebar titles
    
    * Add documentation
    
    * Style
    
    * Style
    
    * Remove uneeded return

---
+++ layouts/Layout.vue
@@ -0,0 +1,32 @@
+<template>
+  <ParentLayout>
+    <template #sidebar-top v-if="sidebarTitle">
+      <h2 class="sidebar-title" v-text="sidebarTitle" />
+    </template>
+  </ParentLayout>
+</template>
+
+<script>
+import ParentLayout from '@parent-theme/layouts/Layout.vue'
+
+export default {
+  name: 'Layout',
+  components: {
+    ParentLayout,
+  },
+  computed: {
+    sidebarTitle: () => {
+      for (let [group, title] of Object.entries(this.$site.themeConfig.sidebarTitles || {})) {
+        if (this.$page.path.startsWith(group)) {
+          return title
+        }
+      }
+    }
+  }
+}
+</script>
+<style lang="stylus">
+.sidebar-title
+  padding-left 1.25rem
+  padding-bottom 0
+</style>
